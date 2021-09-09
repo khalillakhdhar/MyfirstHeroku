82ea231fce7653e9e6169926e53f1386966079a5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 13:28:59 +0100

    code

--- src/main/java/com/http/tp/HelloController.java
+++ src/main/java/com/http/tp/HelloController.java
@@ -2,6 +2,8 @@ package com.http.tp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.http.tp.classes.Livre;
 @Controller
 
 public class HelloController {
@@ -12,5 +14,12 @@ public String index(Model model) { // add `message`
 model.addAttribute("message", " ceci est un message dynamique.");
 // return view name
 return "index";
+}
+@GetMapping("/livre")
+public String livres(Model m)
+{
+	Livre l=new Livre("HTML FOR KIDS","jack pro", 20);
+m.addAttribute("livre",l.toString());
+return "livre";
 }
 }
