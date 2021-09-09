fa806410126a552b7a8c85df8c877a5c6a962a7a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 11:14:17 +0100

    Create HelloController.java

---
+++ src/main/java/com/http/tp/HelloController.java
@@ -0,0 +1,13 @@
+package com.http.tp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+@GetMapping("")
+public String welcome()
+{
+return "heroku is simple to use";	
+}
+}
