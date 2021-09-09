d745501cf7d4d5f36fd88c0ceaaccc4417ba4ed9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-09 11:12:08 +0100

    Initial commit

---
+++ src/main/java/com/http/tp/MyfirstHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.http.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstHerokuApplication.class, args);
+	}
+
+}
